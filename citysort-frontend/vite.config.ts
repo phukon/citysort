6eff9b04308e04983bb4b5bc3d6d587c36d6e903
phukon <rikiphukon16@gmail.com>
2023-11-01 01:00:50 +0530

    feat(initial): add project structure and files

---
+++ citysort-frontend/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'node:path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '~/stitches.config': path.resolve(__dirname, '.stiches.config.js'),
+    },
+  },
+})
