118709a40621eb442c53949fbea72db38b716100
phukon <rikiphukon16@gmail.com>
2023-11-22 01:22:00 +0530

    feat: add analytics

--- citysort-frontend/src/main.tsx
+++ citysort-frontend/src/main.tsx
@@ -4,9 +4,20 @@ import App from './App.tsx';
 import './index.css';
 import Int from './Int.jsx';
 
+import { PostHogProvider } from 'posthog-js/react';
+
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+};
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
+      <App />
+    </PostHogProvider>
     <Int />
   </React.StrictMode>
 );
